47fe4e428ebd78b407bfec69f618ee077c19ad30
ammaralmansor <ammarend@hotmail.com>
2021-08-21 04:43:10 +0300

    share data in all templates with context proccessor

--- e_commerce_env/E_commerce/main/views.py
+++ e_commerce_env/E_commerce/main/views.py
@@ -27,61 +27,39 @@ def brand_list(request):
 
 def product_list(request):
     data = Product.objects.all().order_by('-id')
-    cats = Product.objects.distinct().values('category__title', 'category__id')
-    brands = Product.objects.distinct().values('brand__title', 'brand__id')
-    sizes = Product.objects.distinct().values('size__title', 'size__id')
-    colors = Product.objects.distinct().values(
-        'color__title', 'color__id', 'color__color_code')
+    #min_price = ProductAttribute.objects.aggregate(min('price'))
+    #max_price = ProductAttribute.objects.aggregate(max('price'))
 
     return render(request, 'product_list.html', context={
         'data': data,
-        'cats': cats,
-        'brands': brands,
-        'color': colors,
-        'size': sizes,
-
+        # 'min_price':min_price,
+        # 'max_price':max_price,
     })
 
-#product list according to category
-def category_product_list(request,cat_id):
+# product list according to category
+
+
+def category_product_list(request, cat_id):
     category = Category.objects.get(id=cat_id)
-    data = Product.objects.filter(category= category).order_by('-id')
-    cats = Product.objects.distinct().values('category__title', 'category__id')
-    brands = Product.objects.distinct().values('brand__title', 'brand__id')
-    sizes = Product.objects.distinct().values('size__title', 'size__id')
-    colors = Product.objects.distinct().values(
-        'color__title', 'color__id', 'color__color_code')
-
+    data = Product.objects.filter(category=category).order_by('-id')
     return render(request, 'category_product_list.html', context={
         'data': data,
-        'cats': cats,
-        'brands': brands,
-        'color': colors,
-        'size': sizes,
-
     })
 
-#product list according to brand
-def brand_product_list(request,brand_id):
+# product list according to brand
+
+
+def brand_product_list(request, brand_id):
     brand = Brand.objects.get(id=brand_id)
-    data = Product.objects.filter(brand= brand).order_by('-id')
-    cats = Product.objects.distinct().values('category__title', 'category__id')
-    brands = Product.objects.distinct().values('brand__title', 'brand__id')
-    sizes = Product.objects.distinct().values('size__title', 'size__id')
-    colors = Product.objects.distinct().values(
-        'color__title', 'color__id', 'color__color_code')
+    data = Product.objects.filter(brand=brand).order_by('-id')
 
     return render(request, 'category_product_list.html', context={
         'data': data,
-        'cats': cats,
-        'brands': brands,
-        'color': colors,
-        'size': sizes,
-
     })
 
-#Product Detail
-def product_detail(request,slug,id):
-    data = Product.objects.get(id=id)
-    return render(request,'product_detail.html',{'data':data})
+# Product Detail
 
+
+def product_detail(request, slug, id):
+    data = Product.objects.get(id=id)
+    return render(request, 'product_detail.html', {'data': data})
